00e2c93a72c3f6121886d302da948f2b86fa3a60
7owner <131682352+7owner@users.noreply.github.com>
2025-10-08 08:47:49 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const db = require('./db'); // Import our new database module
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 app.use(cors());
 app.use(express.json());
